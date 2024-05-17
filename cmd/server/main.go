eb7aaf1d95ccee6d36f9ffb5631f4ccbc870a60d
Eduardo Hernandes <eduardodeoh@gmail.com>
2024-05-17 20:02:33 -0300

    Add Postgresql

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"os"
+	"time"
 
-	"github.com/eduardodeoh/go-proc/internal/core/config"
+	"github.com/eduardodeoh/go-poc/internal/core/config"
+	"github.com/eduardodeoh/go-poc/internal/infra/postgresql"
 )
 
 func main() {
@@ -16,6 +19,17 @@ func main() {
 		os.Exit(1)
 	}
 
-	fmt.Println(appConfig)
+	// Initialize Database Pool
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
 
+	db, err := postgresql.NewPool(ctx, appConfig.DbDsn())
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to initialize database pool: %v", err)
+		os.Exit(1)
+	}
+	defer db.Close()
+
+	fmt.Println("Database connected successfully!")
 }
